dba67f4c7760782df9ee59dd166649aea36ddb24
Marco Besier <marcobesier@hey.com>
2023-10-10 16:31:20 +0200

    Complete w01-03

---
+++ src/SanctionsToken.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+
+/**
+ * @title SanctionsToken
+ * @author Marco Besier
+ * @notice This contract implements an ERC-20 token that allows the contract admin to mint tokens to an arbitrary
+ * address. Furthermore, the contract admin can ban specified addresses from sending and receiving tokens.
+ */
+contract SanctionsToken is ERC20 {
+    address public immutable admin;
+    mapping(address => bool) public banned;
+
+    event Ban(address indexed account);
+
+    error NotAdmin();
+    error SenderBanned();
+    error RecipientBanned();
+    error AccountAlreadyBanned();
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        admin = msg.sender;
+    }
+
+    /**
+     * @dev Mints new tokens and assigns them to the specified account.
+     * Only the admin is allowed to call this function.
+     * Emits a {Transfer} event with `from` set to the zero address.
+     * @param to The account that will receive the minted tokens.
+     * @param value The amount of tokens to mint in units of the smallest denomination.
+     */
+    function mint(address to, uint256 value) external {
+        if (msg.sender != admin) {
+            revert NotAdmin();
+        }
+        _mint(to, value);
+    }
+
+    /**
+     * @dev Bans an account from using the token.
+     * @param account The address of the account to be banned.
+     * Emits a {Ban} event.
+     * Requirements:
+     * - The sender must be the admin.
+     * - The account must not already be banned.
+     */
+    function ban(address account) external {
+        if (msg.sender != admin) {
+            revert NotAdmin();
+        }
+        if (banned[account]) {
+            revert AccountAlreadyBanned();
+        }
+        banned[account] = true;
+        emit Ban(account);
+    }
+
+    /**
+     * @dev Overrides the transfer function to check if the sender or recipient is banned before executing the
+     * transfer.
+     * @param to The address of the recipient.
+     * @param value The amount of tokens to transfer in units of the smallest denomination.
+     * @return A boolean value indicating whether the transfer was successful.
+     */
+    function transfer(address to, uint256 value) public override returns (bool) {
+        if (banned[msg.sender]) {
+            revert SenderBanned();
+        }
+        if (banned[to]) {
+            revert RecipientBanned();
+        }
+        return super.transfer(to, value);
+    }
+}
---
+++ test/SanctionsToken.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {SanctionsToken} from "../src/SanctionsToken.sol";
+
+contract SanctionsTokenTest is Test {
+    SanctionsToken public sanctionsToken;
+    address public admin;
+    address public user1;
+    address public user2;
+
+    event Ban(address indexed account);
+
+    error NotAdmin();
+    error SenderBanned();
+    error RecipientBanned();
+    error AccountAlreadyBanned();
+
+    function setUp() public {
+        sanctionsToken = new SanctionsToken("SanctionsToken", "ST");
+        admin = address(this);
+        user1 = address(0x123);
+        user2 = address(0x456);
+    }
+
+    function testFuzz_MintTokens(uint256 value) public {
+        sanctionsToken.mint(user1, value);
+        assertEq(sanctionsToken.balanceOf(user1), value);
+    }
+
+    function testFuzz_OnlyAdminCanMint(address notAdmin) public {
+        vm.assume(notAdmin != admin);
+        vm.prank(notAdmin);
+        vm.expectRevert(abi.encodeWithSelector(NotAdmin.selector));
+        sanctionsToken.mint(notAdmin, 1);
+    }
+
+    function testFuzz_BanAccount(address account) public {
+        vm.assume(account != admin);
+        sanctionsToken.ban(account);
+        assertEq(sanctionsToken.banned(account), true);
+    }
+
+    function testFuzz_OnlyAdminCanBan(address notAdmin) public {
+        vm.assume(notAdmin != admin);
+        vm.prank(notAdmin);
+        vm.expectRevert(abi.encodeWithSelector(NotAdmin.selector));
+        sanctionsToken.ban(notAdmin);
+    }
+
+    function testFuzz_CannotBanTwice(address account) public {
+        vm.assume(account != admin);
+        sanctionsToken.ban(account);
+        vm.expectRevert(abi.encodeWithSelector(AccountAlreadyBanned.selector));
+        sanctionsToken.ban(account);
+    }
+
+    function test_BanEvent() public {
+        vm.expectEmit();
+        emit Ban(user1);
+        sanctionsToken.ban(user1);
+    }
+
+    function testFuzz_Transfer(uint256 value) public {
+        sanctionsToken.mint(user1, value);
+        vm.prank(user1);
+        sanctionsToken.transfer(user2, value);
+        assertEq(sanctionsToken.balanceOf(user1), 0);
+        assertEq(sanctionsToken.balanceOf(user2), value);
+    }
+
+    function testFuzz_SenderBanned(address banned) public {
+        vm.assume(banned != user1);
+        sanctionsToken.mint(banned, 1);
+        sanctionsToken.ban(banned);
+        vm.prank(banned);
+        vm.expectRevert(abi.encodeWithSelector(SenderBanned.selector));
+        sanctionsToken.transfer(user1, 1);
+    }
+
+    function testFuzz_RecipientBanned(address banned) public {
+        vm.assume(banned != user2);
+        sanctionsToken.mint(user1, 1);
+        sanctionsToken.ban(banned);
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(RecipientBanned.selector));
+        sanctionsToken.transfer(banned, 1);
+    }
+}
