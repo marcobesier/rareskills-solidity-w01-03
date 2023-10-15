96e6a9c8710aece654de070e245279911a247a1a
Marco Besier <marcobesier@hey.com>
2023-10-15 16:28:51 +0200

    Use modifiers for admin and banned checks

--- src/SanctionsToken.sol
+++ src/SanctionsToken.sol
@@ -20,6 +20,23 @@ contract SanctionsToken is ERC20 {
     error RecipientBanned();
     error AccountAlreadyBanned();
 
+    modifier onlyAdmin() {
+        if (msg.sender != ADMIN) {
+            revert NotAdmin();
+        }
+        _;
+    }
+
+    modifier notBanned(address from, address to) {
+        if (banned[from]) {
+            revert SenderBanned();
+        }
+        if (banned[to]) {
+            revert RecipientBanned();
+        }
+        _;
+    }
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         ADMIN = msg.sender;
     }
@@ -31,10 +48,7 @@ contract SanctionsToken is ERC20 {
      * @param to The account that will receive the minted tokens.
      * @param value The amount of tokens to mint in units of the smallest denomination.
      */
-    function mint(address to, uint256 value) external {
-        if (msg.sender != ADMIN) {
-            revert NotAdmin();
-        }
+    function mint(address to, uint256 value) external onlyAdmin {
         _mint(to, value);
     }
 
@@ -46,10 +60,7 @@ contract SanctionsToken is ERC20 {
      * - The sender must be the admin.
      * - The account must not already be banned.
      */
-    function ban(address account) external {
-        if (msg.sender != ADMIN) {
-            revert NotAdmin();
-        }
+    function ban(address account) external onlyAdmin {
         if (banned[account]) {
             revert AccountAlreadyBanned();
         }
@@ -64,13 +75,19 @@ contract SanctionsToken is ERC20 {
      * @param value The amount of tokens to transfer in units of the smallest denomination.
      * @return A boolean value indicating whether the transfer was successful.
      */
-    function transfer(address to, uint256 value) public override returns (bool) {
-        if (banned[msg.sender]) {
-            revert SenderBanned();
-        }
-        if (banned[to]) {
-            revert RecipientBanned();
-        }
+    function transfer(address to, uint256 value) public override notBanned(msg.sender, to) returns (bool) {
         return super.transfer(to, value);
+    }
+
+    /**
+     * @dev Overrides the transferFrom function to check if the sender or recipient is banned before executing the
+     * transfer.
+     * @param from The address of the sender.
+     * @param to The address of the recipient.
+     * @param value The amount of tokens to transfer in units of the smallest denomination.
+     * @return A boolean value indicating whether the transfer was successful.
+     */
+    function transferFrom(address from, address to, uint256 value) public override notBanned(from, to) returns (bool) {
+        return super.transferFrom(from, to, value);
     }
 }
